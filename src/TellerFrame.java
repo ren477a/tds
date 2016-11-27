c095c7cb55b9230b83c7fa7c52ae09f60e6a05b7
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2016-11-27 13:49:09 +0800

    teller view core features done

--- src/TellerFrame.java
+++ src/TellerFrame.java
@@ -19,7 +19,7 @@ public class TellerFrame extends JFrame {
 				+"FROM tickets " 
 				+"INNER JOIN events " 
 				+"ON tickets.event_id=events.event_id "
-				+"ORDER BY tickets.event_id, tickets.ticket_price DESC";;
+				+"ORDER BY tickets.event_id, tickets.ticket_price DESC";
 		private ButtonListener btnL;
 		
 		private Cart cart;
@@ -37,18 +37,20 @@ public class TellerFrame extends JFrame {
 			listCart.setLayoutOrientation(JList.VERTICAL);
 			listCart.setVisibleRowCount(3);
 			JScrollPane scrCart = new JScrollPane(listCart);
-			scrCart.setAlignmentX(Component.LEFT_ALIGNMENT);
+			scrCart.setAlignmentX(Component.CENTER_ALIGNMENT);
 			JScrollPane scrTickets = new JScrollPane(tblTickets);
 			scrTickets.setAlignmentX(Component.LEFT_ALIGNMENT);
 			lblSelTickets = new JLabel("Select Tickets:");
 			lblCart = new JLabel("Cart:");
 			lblT = new JLabel("Total:");
+			lblT.setAlignmentX(Component.CENTER_ALIGNMENT);
 			lblTotal = new JLabel("0");
+			lblTotal.setAlignmentX(Component.CENTER_ALIGNMENT);
 			lblCashPay = new JLabel("Cash Payment:");
+			lblCashPay.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 			tfCash = new JTextField();
-			tfCash.setAlignmentX(Component.LEFT_ALIGNMENT);
-			tfCash.setMaximumSize(new Dimension(100, 20));
+			tfCash.setColumns(15);
 			
 			btnAdd = new JButton("Add to Cart");
 			btnAdd.addActionListener(btnL);
@@ -58,42 +60,43 @@ public class TellerFrame extends JFrame {
 			btnRemove.addActionListener(btnL);
 			btnSubmit = new JButton("Submit");
 			btnSubmit.addActionListener(btnL);
-			btnSubmit.setEnabled(false);
 			
 			
 			JPanel pnlBtnLeft = new JPanel(new FlowLayout());
 			pnlBtnLeft.setAlignmentX(Component.LEFT_ALIGNMENT);
 			pnlBtnLeft.add(btnAdd);
 			pnlBtnLeft.add(btnViewMore);
-			JPanel pnlLeft = new JPanel();
-			pnlLeft.setLayout(new BoxLayout(pnlLeft, BoxLayout.PAGE_AXIS));
-			pnlLeft.setBorder(new EmptyBorder(20,20,20,20));
-			pnlLeft.add(lblSelTickets);
-			pnlLeft.add(Box.createRigidArea(new Dimension(0, 5)));
-			pnlLeft.add(scrTickets);
-			pnlLeft.add(pnlBtnLeft);
-			
+			JPanel pnlTop = new JPanel();
+			pnlTop.setLayout(new BoxLayout(pnlTop, BoxLayout.PAGE_AXIS));
+			pnlTop.setBorder(new EmptyBorder(20,20,20,20));
+			pnlTop.add(lblSelTickets);
+			pnlTop.add(Box.createRigidArea(new Dimension(0, 5)));
+			pnlTop.add(scrTickets);
+			pnlTop.add(pnlBtnLeft);
+			pnlTop.add(lblCart);
 			JPanel pnlBtnRight = new JPanel(new FlowLayout());
-			pnlBtnRight.setAlignmentX(Component.LEFT_ALIGNMENT);
+			pnlBtnRight.setAlignmentX(Component.CENTER_ALIGNMENT);
 			pnlBtnRight.add(btnRemove);
 			pnlBtnRight.add(btnSubmit);
-			JPanel pnlRight = new JPanel();
-			pnlRight.setLayout(new BoxLayout(pnlRight, BoxLayout.PAGE_AXIS));
-			pnlRight.add(lblCart);
-			pnlRight.add(Box.createRigidArea(new Dimension(0, 5)));
-			pnlRight.add(scrCart);
-			pnlRight.add(Box.createRigidArea(new Dimension(0, 5)));
-			pnlRight.add(lblT);
-			pnlRight.add(lblTotal);
-			pnlRight.add(Box.createRigidArea(new Dimension(0, 5)));
-			pnlRight.add(lblCashPay);
-			pnlRight.add(Box.createRigidArea(new Dimension(0, 5)));
-			pnlRight.add(tfCash);
-			pnlRight.add(pnlBtnRight);
+			JPanel pnlBtm = new JPanel();
+			pnlBtm.setAlignmentX(Component.LEFT_ALIGNMENT);
+			pnlBtm.setLayout(new BoxLayout(pnlBtm, BoxLayout.PAGE_AXIS));
+			pnlBtm.add(scrCart);
+			pnlBtm.add(Box.createRigidArea(new Dimension(0, 5)));
+			JPanel pnlTotal = new JPanel(new FlowLayout());
+			pnlTotal.add(lblT);
+			pnlTotal.add(lblTotal);
+			pnlBtm.add(pnlTotal);
+			pnlBtm.add(Box.createRigidArea(new Dimension(0, 5)));
+			JPanel pnlCash = new JPanel(new FlowLayout());
+			pnlCash.add(lblCashPay);
+			pnlCash.add(tfCash);
+			pnlBtm.add(pnlCash);
+			pnlBtm.add(pnlBtnRight);
 			
-			pnlLeft.add(pnlRight);
+			pnlTop.add(pnlBtm);
 
-			add(pnlLeft);
+			add(pnlTop);
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
 			setTitle("Ticket Master");
 			setSize(600, 600);
@@ -112,7 +115,6 @@ public class TellerFrame extends JFrame {
 							cart.addToCart(new Ticket(id), qty);
 							mdlCart.addElement(cart.getLastDisplaySummary());
 							lblTotal.setText(Double.toString(cart.getTotal()));
-							btnSubmit.setEnabled(true);
 						} catch (NumberFormatException e) {
 
 						}
@@ -123,8 +125,6 @@ public class TellerFrame extends JFrame {
 						mdlCart.remove(listCart.getSelectedIndex());
 						cart.removeItem(selected);
 						lblTotal.setText(Double.toString(cart.getTotal()));
-						if(cart.isEmpty())
-							btnSubmit.setEnabled(false);
 					}
 					
 				} else if(ae.getSource().equals(btnViewMore)) {
@@ -142,7 +142,7 @@ public class TellerFrame extends JFrame {
 							cart.submitPurchase(db, cash);
 							tfCash.setText("");
 							mdlCart.removeAllElements();
-							btnSubmit.setEnabled(false);
+							lblTotal.setText("0");
 							db.refreshTable(tblTickets, tblTktsQuery, tblTktCols);
 							
 						}
